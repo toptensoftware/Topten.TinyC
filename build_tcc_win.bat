a4a1c4549ba96913ae8837369fcff6b083fd07a3
Brad Robinson <contact@toptensoftware.com>
2024-01-05 13:44:26 +1100

    Include tccbin build directory in git

--- build_tcc_win.bat
+++ build_tcc_win.bat
@@ -2,13 +2,13 @@ set vcvarsall="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxili
 
 if '%1' == '' (
 
-    rm -rf ./bin/tinycc/win32
+    rm -rf ./tccbin/win32
     cmd /c %0 x86
     cmd /c %0 x64
     
 ) else (
 
-    mkdir .\bin\tinycc\win32\%1
+    mkdir .\tccbin\win32\%1
     call %vcvarsall% %1
     pushd tinycc\win32
     if '%1' == 'x86' (
@@ -16,9 +16,9 @@ if '%1' == '' (
     ) else (
         call build-tcc.bat -t 64 -c cl
     )
-    cp libtcc.dll ../../bin/tinycc/win32/%1
-    cp -r lib ../../bin/tinycc/win32/%1
-    cp -r include ../../bin/tinycc/win32/%1
+    cp libtcc.dll ../../tccbin/win32/%1
+    cp -r lib ../../tccbin/win32/%1
+    cp -r include ../../tccbin/win32/%1
     popd
 
 )
